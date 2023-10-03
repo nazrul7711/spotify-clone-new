4836347f615de0a0bf48d862b2a9d89b51bfab95
Nazrul7711 <nazrul7711@gmail.com>
2023-10-03 11:59:31 +0530

    next config file modified

--- next.config.js
+++ next.config.js
@@ -1,12 +1,28 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
-const nextConfig = {
-  images: {
-    domains: ["nextjs-project-nazrul.s3.ap-south-1.amazonaws.com"],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
+const {PH, PHASE_DEVELOPMENT_SERVER} =require("next/constants")
+const nextConfig = (phase)=>{
+  if(phase===PHASE_DEVELOPMENT_SERVER){
+    return {
+      NEXT_PUBLIC_URL: "https://spotify-clone-new-steel.vercel.app/",
+      images: {
+        domains: ["nextjs-project-nazrul.s3.ap-south-1.amazonaws.com"],
+      },
+      sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+      },
+    };
+  }
+  return {
+    NEXT_PUBLIC_URL:"http://localhost:3000/",
+    images: {
+      domains: ["nextjs-project-nazrul.s3.ap-south-1.amazonaws.com"],
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+    },
+  };
+
 };
 
 module.exports = nextConfig;
